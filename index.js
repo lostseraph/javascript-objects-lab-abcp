a4ddbfa7a978888f741f3b3f62f68db3b1444618
Roscoe Sabale <rsabale43@gmail.com>
2018-01-20 08:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var object2 = Object.assign({}, object);
-  delete object2.key;
-  return object2;
+  var newob = Object.assign({}, object);
+  delete newob.key;
+  return newob;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
